69ef9105280050fe96f08c67a8847685dc4c6fdd
azoraw <al.zoraw@gmail.com>
2019-09-29 15:36:45 +0200

    - proper request body

--- src/main/java/com/zoraw/cinema/controller/rest/ReservationRest.java
+++ src/main/java/com/zoraw/cinema/controller/rest/ReservationRest.java
@@ -5,10 +5,7 @@ import com.zoraw.cinema.model.dto.ReservationResponseDto;
 import com.zoraw.cinema.model.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/reservations")
@@ -18,7 +15,7 @@ public class ReservationRest {
     private final ReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<ReservationResponseDto> createReservation(@RequestParam ReservationDto reservationDto) {
+    public ResponseEntity<ReservationResponseDto> createReservation(@RequestBody ReservationDto reservationDto) {
         //todo: validate reservationDto
 
         return ResponseEntity.ok(reservationService.create(reservationDto));
