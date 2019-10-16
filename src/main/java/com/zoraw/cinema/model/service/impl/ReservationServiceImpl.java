7c44c166c76596cc2e341b57aa44237fd45fc424
azoraw <al.zoraw@gmail.com>
2019-10-16 23:18:20 +0200

    Removing unnecessary dependency

--- src/main/java/com/zoraw/cinema/model/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/zoraw/cinema/model/service/impl/ReservationServiceImpl.java
@@ -1,14 +1,12 @@
 package com.zoraw.cinema.model.service.impl;
 
-import com.zoraw.cinema.model.db.mongo.ScreeningRepository;
-import com.zoraw.cinema.model.db.mongo.mapper.ScreeningMapper;
 import com.zoraw.cinema.model.domain.Reservation;
 import com.zoraw.cinema.model.domain.ReservationFailureResponse;
 import com.zoraw.cinema.model.domain.ReservationResponse;
 import com.zoraw.cinema.model.domain.Screening;
-import com.zoraw.cinema.model.exception.ScreeningNotFoundException;
 import com.zoraw.cinema.model.service.ReservationCreationService;
 import com.zoraw.cinema.model.service.ReservationService;
+import com.zoraw.cinema.model.service.ScreeningService;
 import com.zoraw.cinema.model.service.TicketPriceCalculationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,14 +22,13 @@ class ReservationServiceImpl implements ReservationService {
     private int minimumMinutesBeforeScreening;
 
     private final ReservationCreationService reservationCreationService;
-    private final ScreeningRepository screeningRepository;
-    private final ScreeningMapper screeningMapper;
+    private final ScreeningService screeningService;
     private final TicketPriceCalculationService ticketPriceCalculationService;
 
     @Override
     public ReservationResponse create(Reservation reservation) {
         String screeningId = reservation.getScreeningId();
-        Screening screening = getScreening(screeningId);
+        Screening screening = screeningService.getScreening(screeningId);
 
         if (isReservationTooLate(screening)) {
             return ReservationFailureResponse.createTooLateResponse();
@@ -46,19 +43,11 @@ class ReservationServiceImpl implements ReservationService {
                     .build();
         }
 
-        return ReservationFailureResponse.createBadSeatConfigurationResponse(getScreening(screeningId));
+        return ReservationFailureResponse.createBadSeatConfigurationResponse(screeningService.getScreening(screeningId));
     }
 
     private boolean isReservationTooLate(Screening screening) {
         return LocalDateTime.now()
                 .isAfter(screening.getTime().minusMinutes(minimumMinutesBeforeScreening));
     }
-
-    private Screening getScreening(String screeningId) {
-        return screeningMapper.toScreening(
-                screeningRepository.findById(screeningId)
-                        .orElseThrow(() -> new ScreeningNotFoundException(screeningId)));
-    }
-
-
 }
